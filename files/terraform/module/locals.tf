8359849ff141c8c99da33f156b5837ee43d66eb8
Alexander <kvendingoldo@yandex.ru>
2022-08-28 04:15:49 +0400

    fix: TF locals

--- files/terraform/module/locals.tf
+++ files/terraform/module/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  events = {for event in var.events : event["cognitoUserPoolID"] => event if var.cron_enabled}
+  events = {for event in var.events : event["cognitoUserPoolId"] => event if var.cron_enabled}
 }
